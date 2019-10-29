4e086bba9af2ff7365d76e18a589e0c01cb3cd10
Julius <test>
2019-10-29 19:04:43 +0200

    fixed css a bit, default pics

--- src/components/listTest.vue
+++ src/components/listTest.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
     <div class="Test">
-      <img :src="message.image_url" class="image1" width="100px" />
+      <img v-show="message.image_url"
+        :src="
+          message.image_url ||
+            'http://www.thesportsfannetwork.com/siteresources/images/defaultavatar.jpg'
+        "
+        class="image1"
+        width="120px"
+        height="110px"
+      />
       <img
-        :src="message.current_team.image_url"
+        :src="
+          message.current_team.image_url ||
+            'https://a.espncdn.com/i/teamlogos/soccer/500/default-team-logo-500.png'
+        "
         width="100px"
         height="100px"
         class="image2"
       />
-      <div class="team-name">Team Name: {{ message.current_team.name }}</div>
-      <div class="game-name">Game Name: {{ message.name }}</div>
-      <div class="real-name">
-        Real Name: {{ message.first_name }} {{ message.last_name }}
-      </div>
+      <div class="team-name">{{ message.current_team.name }}</div>
+      <div class="game-name">{{ message.name }}</div>
     </div>
   </div>
 </template>
@@ -26,37 +34,38 @@ export default {
 };
 </script>
 <style>
-/* Three image containers (use 25% for four, and 50% for two, etc) */
 .Test {
   width: 500px;
   height: 150px;
   border: 3px ridge #6b6b6b;
-  border-radius: 40px;
-  background: #565354 none no-repeat scroll 25px 24px;
+  border-radius: 10px;
+  background: linear-gradient( to right,#5D5C61,#7e9a9a);
 }
 .team-name {
   position: relative;
   top: -70px;
   left: 20%;
+  font-family: Arial;
+  font-size: 15px;
+  color: rgba(255, 255, 255, 0.5);
 }
 .game-name {
   position: relative;
   top: -50px;
-  left: 20%;
-}
-.real-name {
-  position: relative;
-  top: -30px;
-  left: 20%;
+  left: 10%;
+  font-family: Arial;
+  font-size: 30px;
+  color: rgba(255, 255, 255, 0.5);
 }
 .image1 {
   position: relative;
-  left: -7%;
+  left: -17%;
   top: 30px;
+  z-index: 1;
 }
 .image2 {
   position: relative;
-  left: -49%;
+  left: -30%;
   top: 30px;
 }
 </style>
